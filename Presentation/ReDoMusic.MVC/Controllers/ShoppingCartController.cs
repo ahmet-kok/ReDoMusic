47862ce1ecdab19c7e4bb8262b8631baf78e129e
kadirhan <61658086+Kadirhan54@users.noreply.github.com>
2023-10-30 15:02:30 +0300

    home view added & add cart item endpoint completed

--- Presentation/ReDoMusic.MVC/Controllers/ShoppingCartController.cs
+++ Presentation/ReDoMusic.MVC/Controllers/ShoppingCartController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ReDoMusic.Domain.Entites;
+using ReDoMusic.Domain.Enums;
 using ReDoMusic.Persistence.Context;
 
 namespace ReDoMusic.MVC.Controllers
@@ -13,51 +14,71 @@ namespace ReDoMusic.MVC.Controllers
             _context = new();
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             var shoppingCart = _context.ShoppingCarts.FirstOrDefault();
 
-            Instrument instrument = new Instrument
-            {
-                Id = Guid.NewGuid(), // Assign a unique identifier
-                Name = "Example Instrument",
-                Brand = new Brand
-                {
-                    Id = Guid.NewGuid(), // Assign a unique identifier for the Brand
-                    Name = "Example Brand",
-                    DisplayText = "Example Display Text",
-                    Address = "123 Example Street"
-                },
-                Model = "Example Model",
-                Color = Domain.Enums.Color.Red, // Assuming Color is an enum
-                ProductionYear = new DateTime(2023, 1, 1),
-                Price = 999.99m
-            };
+            //Instrument instrument = new Instrument
+            //{
+            //    Id = Guid.NewGuid(), // Assign a unique identifier
+            //    Name = "Example Instrument",
+            //    Brand = new Brand
+            //    {
+            //        Id = Guid.NewGuid(), // Assign a unique identifier for the Brand
+            //        Name = "Example Brand",
+            //        DisplayText = "Example Display Text",
+            //        Address = "123 Example Street"
+            //    },
+            //    Model = "Example Model",
+            //    Color = Domain.Enums.Color.Red, // Assuming Color is an enum
+            //    ProductionYear = new DateTime(2023, 1, 1),
+            //    Price = 999.99m
+            //};
 
 
-            shoppingCart.Items.Add(instrument);
+            //shoppingCart.Items.Add(instrument);
             return View(shoppingCart);
         }
 
         [HttpGet]
-        public IActionResult AddBrand()
+        public IActionResult AddCartItem()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult AddCartItem(string name, string brand, string brandAddress)
+        public IActionResult AddCartItem(Guid id,string name, string brand, Color color, decimal price, string model)
         {
-            //var brand = new Instrument()
+            //var brand2 = new Brand
+            //{
+            //    Id = Guid.NewGuid(), // Assign a unique identifier for the Brand
+            //    Name = "Example Brand",
+            //    DisplayText = "Example Display Text",
+            //    Address = "123 Example Street"
+            //};
+
+            //Instrument instrument = new()
             //{
             //    Id = Guid.NewGuid(),
-            //    Name = brandName,
-            //    Address = brandAddress,
-            //    DisplayText = brandDisplayText,
-            //    CreatedOn = DateTime.UtcNow,
+            //    Name = name,
+            //    Brand = brand2,
+            //    Price = price,
+            //    Color = color,
+            //    Model = model
             //};
-            //_context.Add(brand);
-            //_context.SaveChanges();
+
+            var instrument = _context.Instruments.Where(x => x.Id == id).FirstOrDefault();
+
+            if (instrument==null)
+                return BadRequest();
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.Items.Add(instrument);
+
+            _context.ShoppingCarts.Add(shoppingCart);
+            _context.SaveChanges();
+
             return View();
         }
     }
