9e5b79959eb3458d73c2ce0d8977534f38ce059d
Savcdilara <savcdilara@gmail.com>
2023-11-01 23:11:36 +0300

    Contact Created
    Add Contact form

---
+++ Core/ReDoMusic.Domain/Entites/Contact.cs
@@ -0,0 +1,21 @@
+﻿using ReDoMusic.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReDoMusic.Domain.Entities
+{
+    public class Contact : EntityBase<Guid>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public bool IsResolved { get; set; }
+
+    }
+
+}
